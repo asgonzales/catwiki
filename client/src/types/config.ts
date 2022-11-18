401a640be048f0141db6d5fa1b20af0963c6bb82
asgonzales <asgonzalesromani@gmail.com>
2022-11-17 22:03:39 -0300

    chore: Preparing for deploy

--- client/src/types/config.ts
+++ client/src/types/config.ts
@@ -1,6 +1,6 @@
 // import dotenv from 'dotenv';
 // dotenv.config();
-
+console.log(process.env.REACT_APP_BASE_URL)
 export const config = {
-    backUrl: process.env.REACT_APP_BASE_URL || 'http://localhost:3001'
+    backUrl: process.env.REACT_APP_BASE_URL
 }
