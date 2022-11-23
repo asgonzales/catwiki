1f9cf1c730937fa88604b85b623a26f9ff88fdef
asgonzales <asgonzalesromani@gmail.com>
2022-11-23 19:54:49 -0300

    feat: Add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Pages/Home/Home';
 import BreedDetail from './components/Pages/BreedDetail/BreedDetail';
 import Game from './components/Pages/Game';
-import About from './components/Pages/About';
+import About from './components/Pages/About/About';
 import NotFound from './components/Pages/NotFound/NotFound';
 import Results from './components/Pages/Results/Results';
 import SearchBar from './components/SearchBar/SearchBar';
 import backImg from './media/Icons/logo.png';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path='/about' element={<About/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
+      <Footer />
     </div>
   );
 }
